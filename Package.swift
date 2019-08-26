b08cc7204e9156e0a1bc90611ebccee3b00ff9ea
John Lima <thejohnlima@icloud.com>
2019-08-25 23:35:47 -0300

    Setup project struct

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+  name: "SWMailgun",
+  platforms: [
+      .macOS(.v10_13),
+      .iOS(.v9),
+      .watchOS(.v3),
+      .tvOS(.v9)
+  ],
+  products: [
+    .library(
+      name: "SWMailgun",
+      targets: ["SWMailgun"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/limadeveloper/BaseNetworkKit.git", from: "1.0.9")
+  ],
+  targets: [
+    .target(
+      name: "SWMailgun",
+      dependencies: ["BaseNetworkKit"]
+    ),
+    .testTarget(
+      name: "SWMailgunTests",
+      dependencies: ["SWMailgun"]
+    ),
+  ]
+)
